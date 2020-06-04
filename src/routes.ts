4cf5cb83e7b3f67da02d095870dfde3a0f0a0ae6
Paulo Borges <pborges086@hotmail.com>
2020-06-04 01:24:37 -0300

    Finalizando métodos

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,18 @@
 import express, { Router } from 'express';
+import PointsController from './controller/PoitnsController';
+import ItemsController from './controller/ItemsController';
 
 const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get('/', (request, response) => {
-    return response.json({ message: "Hello World!" });
-});
+
+routes.get('/items', itemsController.index);
+
+routes.post('/points', pointsController.create);
+
+routes.get('/points/:id', pointsController.show);
+
+routes.get('/points', pointsController.index);
 
 export default routes;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import express, { request, response } from 'express';
+import path from 'path';
 import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
 
+app.use(cors());
+
 app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
